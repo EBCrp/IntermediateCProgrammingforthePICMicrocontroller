b8b366a6f29b4419b0cc13757131394bd468842d
EBCrp <117190758+EBCrp@users.noreply.github.com>
2024-02-10 10:29:22 -0500

    Add files via upload

---
+++ stepperMotorMain.c
@@ -0,0 +1,170 @@
+/*
+ * File:   stepperMotorMain.c
+ * Description: Basic Program for the Stepper Motor
+ * Author: Ellery Cadel
+ * Copied from book by H.H. Ward 
+ * Configuration: PIC18F4525
+ * OSC set to INTI067
+ * WDT set to OFF
+ * LVP set to OFF
+ *
+ * Created on February 9, 2024
+ */
+
+#include <conFigInternalOscNoWDTNoLVP.h>
+#include <xc.h>
+#include <PICSetUp.h>
+
+
+// Some definitions
+
+#define orange PORTBbits.RB3
+#define yellow PORTBbits.RB2
+#define pink PORTBbits.RB1
+#define blue PORTBbits.RB0
+
+// Global variables
+
+unsigned char n, speed, clkcount;
+unsigned int ck;
+
+// Subroutines
+
+void delay (unsigned char t)
+{
+    while (clkcount < t)
+    {
+        TMR0 = 0;
+        while (TMR0 < 250);
+        clkcount ++;
+        
+        
+    }
+}
+
+void main ()
+{
+    initialize ();
+    T0CON = 0xC6;
+    speed = 50;
+    while (1)
+    {
+        if (PORTAbits.RA0) speed = 75;
+        if (PORTAbits.RA1) speed = 40; 
+        if (PORTAbits.RA2) speed = 20;
+        ck = 0;
+        clockwise: while (ck < 400)
+        {
+            orange = 1;
+            yellow = 0;
+            pink = 0;
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 0;
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 1;
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 1;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 1;
+            pink = 1;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 1;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 1;
+            yellow = 1;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 1;
+            yellow = 0;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            ck ++;
+            
+            goto clockwise;
+        }
+        ck = 0;
+        anticlockwise: while (ck < 500)
+        {
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 1;
+            yellow = 0;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 1;
+            yellow = 1;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 1;
+            pink = 0;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 1;
+            pink = 1;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 1;
+            blue = 0;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 1;
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 0;
+            yellow = 0;
+            pink = 0;
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            orange = 1;
+            yellow = 0;
+            pink = 0; // line missing in book
+            blue = 1;
+            TMR0 = 0;
+            while (TMR0 < speed);
+            ck ++;
+            goto anticlockwise;
+        }
+        clkcount = 0;
+        delay (16);
+    }
+}
+
